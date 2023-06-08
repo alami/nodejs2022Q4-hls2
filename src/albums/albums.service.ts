f29e6839897797dbcc39df3962b03e510d1e97bc
aminov <alami@tshtt.uz>
2023-06-08 16:12:29 +0500

    add db entities:album

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -3,67 +3,43 @@ import { AlbumsDto } from './dto/albums.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { Album, Track } from '../models/interfaces';
 import { DbService } from '../models/db.service';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AlbumsEntity } from './entities/album.entity';
 
 @Injectable()
 export class AlbumsService {
-  constructor(@Inject(DbService) private db: DbService) {}
+  constructor(
+      @InjectRepository(AlbumsEntity)
+      private albumRepo: Repository<AlbumsEntity>,
+      @Inject(DbService) private db: DbService
+  ) {}
 
   getAll() {
-    return this.db.albums;
+    return this.albumRepo.find();
   }
 
   getOneById(id: string) {
-    const album = this.db.albums.find((el) => el.id === id);
-    return album;
+    return this.albumRepo.findOne({where: {id}});
   }
 
   create(dto: AlbumsDto) {
-    const newAlbum = { ...dto } as Album;
-    newAlbum.id = uuidv4();
-    const artist = this.db.artists.find((el) => el.id === dto.artistId);
-    if (artist === undefined && dto.artistId !== null) {
-      return undefined;
-    }
-    this.db.albums.push(newAlbum);
-    return newAlbum;
+    const newAlbum = { id: uuidv4(), ...dto } as Album;
+    const album = this.albumRepo.create(newAlbum);
+    return this.albumRepo.save(album);
   }
 
-  updateOne(id: string, dto: AlbumsDto) {
-    const album = this.db.albums.find((el) => el.id === id);
-    if (album === undefined) {
-      return album;
-    }
-    const albumIndex = this.db.albums.findIndex((el) => el.id === id);
-    const artist = this.db.artists.find((el) => el.id === dto.artistId);
-    if (artist === undefined && dto.artistId !== null) {
-      return undefined;
-    }
+  async updateOne(id: string, dto: AlbumsDto) {
+    const album =  await this.albumRepo.findOne({where: {id}});
     const updAlbum = { ...album, ...dto } as Album;
-    this.db.albums.splice(albumIndex, 1, updAlbum);
-    return updAlbum;
+    return this.albumRepo.save(updAlbum);
   }
 
-  deleteAlbum(id: string) {
-    const album = this.db.albums.find((el) => el.id === id);
+  async deleteAlbum(id: string) {
+    const album = await  this.albumRepo.findOne({where: {id}});
     if (album === undefined) {
       return undefined;
     }
-
-    const favAlbumIndex = this.db.favorites.albums.findIndex(
-      (el) => el.id === album.id,
-    );
-    if (favAlbumIndex !== -1) {
-      this.db.favorites.albums.splice(favAlbumIndex, 1);
-    }
-    const trackInd = this.db.tracks.findIndex((el) => el.albumId === id);
-    const track = this.db.tracks.find((el) => el.albumId === id);
-    if (trackInd !== -1) {
-      const obj = { ...track } as Track;
-      obj.albumId = null;
-      this.db.tracks.splice(trackInd, 1, obj);
-    }
-    const albumIndex = this.db.albums.findIndex((el) => el.id === id);
-    this.db.albums.splice(albumIndex, 1);
-    return album;
+    await this.albumRepo.delete(id);
   }
 }
